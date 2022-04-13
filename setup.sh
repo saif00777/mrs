1a5611448664ef9b467db2113f9474fc57993720
saif00777 <95517674+saif00777@users.noreply.github.com>
2022-04-13 21:18:28 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,10 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[general]\n\
-email = \"saifalikhannda@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
 [server]\n\
-headless = true\n\
-enableCORS=false\n\
 port = $PORT\n\
+enableCORS = false\n\
+headless = true\n\
+     
+\n\      
 " > ~/.streamlit/config.toml
